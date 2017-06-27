78f87abbff644c08b4fa1e87ff6acfba82cefa82
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2017-06-27 21:23:01 +1000

    Config tests for set and InvalidArgumentExceptions

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -7,7 +7,7 @@ use Snout\Exceptions\ConfigException;
 
 class ConfigTest extends TestCase
 {
-    public function testConfig()
+    public function testGet()
     {
         $test_config = __DIR__ . '/test_config.json';
         $config = new Config($test_config);
@@ -21,6 +21,27 @@ class ConfigTest extends TestCase
         $this->assertTrue($snap[2]);
         $this->assertFalse($snap[3]);
 
+        $test = $config->get(['dog', 'cat']);
+        $this->assertEquals(5678, $test['dog']);
+        $this->assertEquals(9123, $test['cat']);
+    }
+
+    public function testSet()
+    {
+        $test_config = __DIR__ . '/test_config.json';
+        $config = new Config($test_config);
+
+        $config->set('pig', 1234);
+        $this->assertEquals(1234, $config->get('pig'));
+
+        $config->set([
+            'seal' => 5678,
+            'bird' => 9123
+        ]);
+
+        $test = $config->get(['seal', 'bird']);
+        $this->assertEquals(5678, $test['seal']);
+        $this->assertEquals(9123, $test['bird']);
     }
 
     /**
@@ -48,7 +69,28 @@ class ConfigTest extends TestCase
     {
         $test_config = __DIR__ . '/test_config.json';
         $config = new Config($test_config);
+        $config->get('crack', true);
+    }
 
-        $this->assertEquals('foo', $config->get('crack', true));
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetInvalidArgument()
+    {
+        $test_config = __DIR__ . '/test_config.json';
+        $config = new Config($test_config);
+
+        $config->get(1234);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetInvalidArgument()
+    {
+        $test_config = __DIR__ . '/test_config.json';
+        $config = new Config($test_config);
+
+        $config->set(1234);
     }
 }
