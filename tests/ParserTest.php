5943777724b03701ba61e45228ca7ad6d79cd759
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2017-07-23 12:04:18 +1000

    Expanded tests

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -1,10 +1,11 @@
 <?php
 namespace Snout\Tests;
 
-use \PHPUnit\Framework\TestCase;
+use Ds\Map;
+use PHPUnit\Framework\TestCase;
+use Snout\Exceptions\ConfigurationException;
 use Snout\Exceptions\ParserException;
 use Snout\Token;
-use Snout\Config;
 use Snout\Lexer;
 use Snout\Parser;
 
@@ -16,19 +17,35 @@ class ParserTest extends TestCase
             __DIR__ . '/configs/test.json'
         );
 
-        $parser = new Parser($config, new Lexer("foo1234/_-:{}\\"));
+        $parser = new Parser(
+            $config->get('parser'),
+            new Lexer("foo1234/_-.:{}\\")
+        );
+
         $this->assertFalse($parser->isEnd());
         $this->assertNull($parser->accept(Token::ALPHA));
         $this->assertNull($parser->accept(Token::DIGIT));
         $this->assertNull($parser->accept(Token::FORWARD_SLASH));
         $this->assertNull($parser->accept(Token::UNDERSCORE));
         $this->assertNull($parser->accept(Token::HYPHEN));
+        $this->assertNull($parser->accept(Token::PERIOD));
         $this->assertNull($parser->accept(Token::COLON));
         $this->assertNull($parser->accept(Token::OPEN_BRACE));
         $this->assertNull($parser->accept(Token::CLOSE_BRACE));
         $this->assertNull($parser->accept(Token::BACK_SLASH));
         $this->assertNull($parser->accept(Token::END));
         $this->assertTrue($parser->isEnd());
+    }
+
+    public function testInvalidConfig() : void
+    {
+        $this->expectException(ConfigurationException::class);
+        $this->expectExceptionMessage('Invalid parser configuration.');
+
+        $parser = new Parser(
+            new Map(),
+            new Lexer(' ')
+        );
     }
 
     public function testInvalidToken() : void
@@ -41,7 +58,11 @@ class ParserTest extends TestCase
             true
         );
 
-        $parser = new Parser($config, new Lexer(' '));
+        $parser = new Parser(
+            $config->get('parser'),
+            new Lexer(' ')
+        );
+
         $parser->accept();
     }
 
@@ -57,7 +78,11 @@ class ParserTest extends TestCase
             true
         );
 
-        $parser = new Parser($config, new Lexer('foo'));
+        $parser = new Parser(
+            $config->get('parser'),
+            new Lexer('foo')
+        );
+
         $parser->accept(Token::DIGIT);
     }
 
@@ -73,7 +98,35 @@ class ParserTest extends TestCase
             true
         );
 
-        $parser = new Parser($config, new Lexer('foo'));
+        $parser = new Parser(
+            $config->get('parser'),
+            new Lexer('foo')
+        );
+
         $parser->accept(Token::ALPHA, 'bar');
+    }
+
+    public function testOptionalAccept() : void
+    {
+        $config = \Snout\json_decode_file(
+            __DIR__ . '/configs/test.json',
+            true
+        );
+
+        $parser = new Parser(
+            $config->get('parser'),
+            new Lexer('.foo')
+        );
+
+        $this->assertEquals(Token::PERIOD, $parser->getToken());
+        $this->assertNull($parser->optionalAccept(Token::ALPHA));
+        $this->assertEquals(Token::PERIOD, $parser->getToken());
+        $this->assertNull($parser->optionalAccept(Token::PERIOD));
+        $this->assertEquals(Token::ALPHA, $parser->getToken());
+        $this->assertNull($parser->optionalAccept(Token::ALPHA, 'bar'));
+        $this->assertEquals(Token::ALPHA, $parser->getToken());
+        $this->assertEquals('foo', $parser->getPayload());
+        $this->assertNull($parser->accept(Token::ALPHA, 'foo'));
+        $this->assertTrue($parser->isEnd());
     }
 }
