83da14f60d24fab40ab99f0eb7f4f5c9f6674464
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2017-10-08 15:33:45 +1100

    Add Request tests

---
+++ tests/RequestTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Snout\Tests;
+
+use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
+use Ds\Map;
+use Snout\Lexer;
+use Snout\Parser;
+use Snout\Request;
+
+class RequestTest extends TestCase
+{
+    public function testGetParser() : void
+    {
+        $request = new Request('/foo', 'post');
+        $this->assertEquals(
+            new Parser(
+                \Snout\array_to_map([
+                    'invalid' => [
+                        'SPACE',
+                        'TAB',
+                        'NEW_LINE',
+                        'CARRIAGE_RETURN'
+                    ]
+                ]),
+                new Lexer('/foo')
+            ),
+            $request->getParser()
+        );
+    }
+
+    public function testGetMethod() : void
+    {
+        $request = new Request('/foo', 'post');
+        $this->assertEquals('post', $request->getMethod());
+    }
+
+    public function testInvalidConfigType() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            '$config must be an array or an instance of \Ds\Map.'
+        );
+
+        $request = new Request('/foo', 'get', 'foo');
+    }
+
+}
